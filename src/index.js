19c993c5c699c4347434c4d7fcf52346a4fc03f7
jodiwarren <jodi@jodiwarren.com>
2018-07-21 16:50:01 +0100

    Beginning of Chapter 6

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/index.css";
+import { ApolloProvider } from "react-apollo";
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  InMemoryCache
+} from "apollo-boost";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
+import { AUTH_TOKEN } from "./constants";
 
-import { ApolloProvider } from "react-apollo";
-import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
-import { BrowserRouter } from "react-router-dom";
+import "./styles/index.css";
 
 const httpLink = new HttpLink({ uri: "http://localhost:4000" });
 
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  const authorizationHeader = token ? `Bearer ${token}` : null;
+  operation.setContext({
+    headers: {
+      authorization: authorizationHeader
+    }
+  });
+  return forward(operation);
+});
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink);
+
 const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: httpLink
+  link: httpLinkWithAuthToken,
+  cache: new InMemoryCache()
 });
 
 ReactDOM.render(
